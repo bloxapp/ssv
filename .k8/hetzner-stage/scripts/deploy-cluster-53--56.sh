5c6bf45ba0ea74364ae17cd5e639011d1714866b
AndrewBlox <andrew@blox.io>
2023-11-17 23:30:28 +0100

    Fork Testing Phase 2

--- .k8/hetzner-stage/scripts/deploy-cluster-53--56.sh
+++ .k8/hetzner-stage/scripts/deploy-cluster-53--56.sh
@@ -107,7 +107,7 @@ DIR=".k8/hetzner-stage"
 DEPLOY_FILES=(
   "ssv-node-53-deployment.yml"
   "ssv-node-54-deployment.yml"
-  # "ssv-node-55-deployment.yml"
+  "ssv-node-55-deployment.yml"
   # "ssv-node-56-deployment.yml"
 )
 
