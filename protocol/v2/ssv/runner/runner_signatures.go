cfe566cbe5622e7590255805415d1fc189e0a456
MatheusFranco99 <48058141+MatheusFranco99@users.noreply.github.com>
2023-12-03 14:16:59 +0000

    Comment unused to pass lint

--- protocol/v2/ssv/runner/runner_signatures.go
+++ protocol/v2/ssv/runner/runner_signatures.go
@@ -4,7 +4,6 @@ import (
 	spec "github.com/attestantio/go-eth2-client/spec/phase0"
 	spectypes "github.com/bloxapp/ssv-spec/types"
 	ssz "github.com/ferranbt/fastssz"
-	"github.com/herumi/bls-eth-go-binary/bls"
 	"github.com/pkg/errors"
 
 	"github.com/bloxapp/ssv/protocol/v2/types"
@@ -65,28 +64,28 @@ func (b *BaseRunner) validatePartialSigMsgForSlot(
 	return nil
 }
 
-func (b *BaseRunner) verifyBeaconPartialSignature(msg *spectypes.PartialSignatureMessage) error {
-	signer := msg.Signer
-	signature := msg.PartialSignature
-	root := msg.SigningRoot
+// func (b *BaseRunner) verifyBeaconPartialSignature(msg *spectypes.PartialSignatureMessage) error {
+// 	signer := msg.Signer
+// 	signature := msg.PartialSignature
+// 	root := msg.SigningRoot
 
-	for _, n := range b.Share.Committee {
-		if n.GetID() == signer {
-			pk, err := types.DeserializeBLSPublicKey(n.GetPublicKey())
-			if err != nil {
-				return errors.Wrap(err, "could not deserialized pk")
-			}
-			sig := &bls.Sign{}
-			if err := sig.Deserialize(signature); err != nil {
-				return errors.Wrap(err, "could not deserialized Signature")
-			}
+// 	for _, n := range b.Share.Committee {
+// 		if n.GetID() == signer {
+// 			pk, err := types.DeserializeBLSPublicKey(n.GetPublicKey())
+// 			if err != nil {
+// 				return errors.Wrap(err, "could not deserialized pk")
+// 			}
+// 			sig := &bls.Sign{}
+// 			if err := sig.Deserialize(signature); err != nil {
+// 				return errors.Wrap(err, "could not deserialized Signature")
+// 			}
 
-			// verify
-			if !sig.VerifyByte(&pk, root[:]) {
-				return errors.New("wrong signature")
-			}
-			return nil
-		}
-	}
-	return errors.New("unknown signer")
-}
+// 			// verify
+// 			if !sig.VerifyByte(&pk, root[:]) {
+// 				return errors.New("wrong signature")
+// 			}
+// 			return nil
+// 		}
+// 	}
+// 	return errors.New("unknown signer")
+// }
